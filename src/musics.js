48aab14cc3b8eb113c33d7bf1f4248593bde095b
Samer A <thedev.samer@gmail.com>
2022-11-06 21:57:47 +0200

    initial commit with starter files

---
+++ src/musics.js
@@ -0,0 +1,67 @@
+const Data = [
+  {
+    id: 1,
+    title: "As You Were - TrackTribe",
+    artist: "TrackTribe",
+    thumbnail: "/assets/images/img01.jpg",
+    src: "/assets/media/As You Were - TrackTribe.mp3",
+  },
+  {
+    id: 2,
+    title: "Coast - Anno Domini Beats",
+    artist: "Anno Domini Beats",
+    thumbnail: "/assets/images/img02.jpg",
+    src: "/assets/media/Coast - Anno Domini Beats.mp3",
+  },
+  {
+    id: 3,
+    title: "Here it Comes - TrackTribe",
+    artist: "TrackTribe",
+    thumbnail: "/assets/images/img03.jpg",
+    src: "/assets/media/Here it Comes - TrackTribe.mp3",
+  },
+  {
+    id: 4,
+    title: "It Was a Time - TrackTribe",
+    artist: "TrackTribe",
+    thumbnail: "/assets/images/img04.jpg",
+    src: "/assets/media/It Was a Time - TrackTribe.mp3",
+  },
+  {
+    id: 5,
+    title: "Mulholland - King Canyon",
+    artist: "King Canyon",
+    thumbnail: "/assets/images/img05.jpg",
+    src: "/assets/media/Mulholland - King Canyon.mp3",
+  },
+  {
+    id: 6,
+    title: "No Mercy - TrackTribe",
+    artist: "TrackTribe",
+    thumbnail: "/assets/images/img06.jpg",
+    src: "/assets/media/No Mercy - TrackTribe.mp3",
+  },
+  {
+    id: 7,
+    title: "Put It - TrackTribe",
+    artist: "TrackTribe",
+    thumbnail: "/assets/images/img07.jpg",
+    src: "/assets/media/Put It - TrackTribe.mp3",
+  },
+  {
+    id: 8,
+    title: "Read All Over - Nathan Moore",
+    artist: "Nathan Moore",
+    thumbnail: "/assets/images/img08.jpg",
+    src: "/assets/media/Read All Over - Nathan Moore.mp3",
+  },
+  {
+    id: 9,
+    title: "Silver Waves - TrackTribe",
+    artist: "TrackTribe",
+    thumbnail: "/assets/images/img09.jpg",
+    src: "/assets/media/Silver Waves - TrackTribe.mp3",
+  },
+];
+
+export default Data;
